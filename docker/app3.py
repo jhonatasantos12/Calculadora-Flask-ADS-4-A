94a9f5825b328db48a598ce407d490dfb791daa6
Jhonatas Santos <37161452+jhonatasantos12@users.noreply.github.com>
2022-04-11 22:20:29 -0300

    atuaalizacao

--- docker/app3.py
+++ docker/app3.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'mudar123'
-app.config['MYSQL_DATABASE_DB'] = 'teste'
-app.config['MYSQL_DATABASE_HOST'] = '172.17.0.7'
+app.config['MYSQL_DATABASE_DB'] = 'meubanco'
+app.config['MYSQL_DATABASE_HOST'] = '172.17.0.2'
 #app.config['MYSQL_DATABASE_HOST'] = '172.17.0.7'
 mysql.init_app(app)
 
